b19fc863f12b8c80e46e145f9065b56f11adc1c5
DTshiyeya <164188333+DTshiyeya@users.noreply.github.com>
2025-11-11 13:03:28 +0200

    Add files via upload

---
+++ src/patientdashboard/PatientDashBoard.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package patientdashboard;
+
+/**
+ *
+ * @author bompe
+ */
+public class PatientDashBoard {
+    
+
+
+    public static void main(String[] args) {
+        splash screen=new splash();
+        screen.setVisible(true);
+        try{
+           
+            for (int i = 0;i <= 100;i++){
+                 Thread.sleep(20);
+                splash.jProgressBar1.setValue(i);
+                if(i==100){
+                    
+                }
+            }
+        }catch(Exception e){
+            
+        }
+    }
+    
+}
